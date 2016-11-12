a6305b5eea53570fb9558fbc9885ee0ece6f6fca
apacheco <j.alfredo.pacheco@gmail.com>
2016-11-12 14:44:16 -0700

    -Adding Backend.

---
+++ backend/Business Specific Logic/EF/SampleContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+namespace BusinessSpecificLogic.EF
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("name=Sample")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+    }
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MainContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MainContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ backend/WEB API Main/Main/Auth/AuthContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ReusableWebAPI.Auth
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext() : base("AuthContext") { }
+    }
+}
